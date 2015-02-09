5b947a6c3d4894c77f3ac2029e7f6e86d8906d1c
dneubaue <d.neubauer@me.com>
2015-02-09 11:13:38 -0600

    Initial Commit

---
+++ iSleepLate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iSleepLate
+//
+//  Created by David Neubauer on 2/9/15.
+//  Copyright (c) 2015 iSleepLate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iSleepLate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iSleepLate
+//
+//  Created by David Neubauer on 2/9/15.
+//  Copyright (c) 2015 iSleepLate. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
