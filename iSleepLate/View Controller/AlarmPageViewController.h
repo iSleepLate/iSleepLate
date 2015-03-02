5cabff9638f065adeb10900eeaeed3660a6dcbdc
dneubaue <d.neubauer@me.com>
2015-03-01 19:53:39 -0600

    Created "AlarmPageViewController" a view controller that displays a page view controller that scrolls between view controllers with different colors.

---
+++ iSleepLate/View Controller/AlarmPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlarmPageViewController.h
+//  iSleepLate
+//
+//  Created by David Neubauer on 3/1/15.
+//  Copyright (c) 2015 iSleepLate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlarmPageViewController : UIViewController
+
+@end
